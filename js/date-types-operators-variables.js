28712d8bb903b783c96ec44c46a38b1a27facbff
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-02-09 14:54:25 -0600

    adding data-operators-variables exercise

---
+++ js/date-types-operators-variables.js
@@ -0,0 +1,175 @@
+let a = 1;
+let b = a++;
+let c = ++a;
+// a = 3, b = 1, c = 3
+console.log(a);
+console.log(b);
+console.log(c);
+
+let d = "hello";
+let e = false;
+d++;
+e++;
+
+let perplexed;
+perplexed + 2;
+console.log(perplexed + 2);
+
+let price = 2.7;
+price.toFixed(2);
+//2.70
+console.log(price.toFixed(2));
+
+let price2 = "2.7";
+price.toFixed(2);
+//only runs numbers
+console.log(price2);
+
+isNaN(0);
+//false
+isNaN(1);
+//false
+isNaN("");
+//true
+isNaN("string");
+//true
+isNaN("0");
+//false
+isNaN("1");
+//false
+isNaN("3.145");
+//false
+isNaN(Number.MAX_VALUE);
+//false
+isNaN(Infinity);
+//false
+isNaN("true");
+//true
+isNaN(true);
+//false
+isNaN("false");
+//true
+isNaN(false);
+//false
+NaN == NaN;
+
+!true;
+//false
+
+!false;
+//true
+
+!!true;
+//true
+
+!!false;
+//false
+
+!!0;
+//true
+
+!!-0;
+//false
+
+!!1;
+//true
+
+!!-1;
+//true
+
+!!0.1;
+//true
+
+!!"hello";
+//true
+
+!!"";
+//false
+
+!!'';
+//false
+
+!!"false";
+//true
+
+!!"0";
+//true
+
+let sample = "Hello Codeup";
+console.log(sample.length);
+
+console.log(sample.toLowerCase());
+console.log(sample.toUpperCase());
+console.log(sample + " Students");
+console.log(sample + " Students".replace(" Students", " Class"));
+console.log(sample.indexOf('c'));
+console.log(sample.indexOf('C'));
+console.log(sample.substring(6));
+
+let firstHalfEnd = sample.indexOf('o') + 2;
+let firstHalfName = sample.substring(0, firstHalfEnd);
+sample = sample.substring(firstHalfEnd)
+console.log(sample);
+
+// let discordName = "[YoLoBlob]Jason";
+// let guildCharEnd = discordName.indexOf(']') + 1;
+// let guildName = discordName.substring(0, guildCharEnd);
+// discordName = discordName.substring(guildCharEnd);
+// let message = `The user's name is ${discordName}.
+// And they are a member of the ${guildName} guild.`;
+// console.log(message);
+// console.log("This user's guild is => ", guildName);
+// console.log("This user's name is => ", discordName);
+let rentMovie = 3
+let mermaid = (3 * rentMovie);
+let brotherBear = (5 * rentMovie);
+let hercules = (1 * rentMovie)
+let totalCost = mermaid + brotherBear + hercules;
+console.log(`Total cost to rent all 3 movies is: $ ${totalCost}`);
+
+let google = (6 * 400);
+let amazon = (4 * 380);
+let facebook = (10 * 350);
+let totalPayWeek = google + amazon + facebook;
+console.log (`My total pay this week is: $ ${totalPayWeek}`);
+
+let classNotFull = true
+let doesNotConflict = true
+let message = "Enrolled!"
+if(classNotFull === doesNotConflict) {
+    console.log(message);
+} else {
+    console.log("Cannot Enroll.");
+}
+
+let itemsBought = 3;
+let isPremium = true;
+let offerExpired = false;
+let offerValid = (isPremium || itemsBought) && offerExpired;
+if(offerValid) {
+    console.log("Discount Applied");
+} else {
+    console.log("Discount Not Appllied");
+}
+// let user = "Jason";
+// let dontLike = "jason";
+// let message = "i don't like you! go away!";
+// message = message.toUpperCase();
+// if(user.toLowerCase() ===dontLike.toLowerCase()) {
+//     console.log(message);
+// } else {
+//     console.log("Not Jason, you're cool.");
+// }
+
+let username = 'codeup';
+let password = 'notastrongpassword';
+let char5 = password >= 5;
+let notUserName = password !== username;
+let userLength = username.length <= 20;
+let noWhitespace = (username.indexOf (" ")) <= 0 && (password.indexOf (" ")) <= 0;
+
+if (char5 && notUserName && userLength && noWhitespace) {
+    console.log("Username and password requirements are met.");
+} else {
+    console.log("Username and password do not meet requirements.");
+}
---
+++ js/javascript-intro.js
@@ -0,0 +1,70 @@
+// /* this is comment
+// and I start again
+//  */
+// // let var1 = true
+// // let var2 = 12345
+// // let var3 = "Hello Codeup"
+// // let var4 = "54321"
+// let var5 = "2" + "2"
+// let var6 = 'single quote string'
+// let var7 = "Jason's been drinking"
+// let var8 = `It wouldn't matter if I "used" single or double`
+//
+// let var9 = 123
+// let var10 = false
+// let var11 = 'true'
+// let var12 = true
+//
+// let var13; //undefined
+//
+// let var14 = 'I like Yamba\'s sweater'
+let var15;
+let var16 = -4346;
+
+let var17;
+let var18 = "Logged in user";
+
+let var19 = 24;
+let var20 = "24";
+
+let userID = 8675309;
+let postAuthorID = 3462353246;
+
+console.log();
+
+let myShoes = "Ariats";
+const myName = "Jason";
+
+myShoes = "Chanclas";
+let counter = 1;
+counter++;
+counter--;
+
+let pie = 3.14;
+
+// pie = pie.tofixed(1);
+// console.log(pie);
+
+// let price = '$' + 21.50.toFixed(2);
+// console.log(price)
+//
+// let students = "24";
+// students = parseInt(students);
+//
+// console.log(students);
+//
+// let price = "24.45";
+// price = parseFloat(price);
+// console.log(price);
+
+
+// let discordName = "[YoLo] Jason";
+// discordName = discordName.split(']');
+// discordName = discordName[1].trim();
+// //"Jason"
+// let shortName = discordName.substring(0, 3);
+// console.log(shortName);
+// let sPosition = discordName.indexOf('s');
+// console.log(discordName.length);
+// discordName = discordName.toLowerCase();
+// discordName =.replace('[yolo]', '');
