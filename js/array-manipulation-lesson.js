b6784e4725b61e55e77184374b81487fa839eee4
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-02-17 14:05:25 -0600

    adding array manipulation lesson and exercies

---
+++ js/array-manipulation-lesson.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+// const myName = "Jason";
+// const sPosition = myName.indexOf('s');
+// console.log(sPosition);
+// const myNameArray = myName.split("");
+// console.log(myNameArray);
+//
+// const thursday = daysOfWeek.indexOf("Thursday");
+// console.log(thursday);
+//
+// const removeSunday = daysOfWeek.pop();
+// console.log(daysOfWeek);
+// console.log(removeSunday);
+//
+// const removeFirst = daysOfWeek.shift();
+// console.log(daysOfWeek);
+// console.log(removeFirst);
+
+/**SLICING**/
+// const firstHalf = daysOfWeek.slice(0, 3);
+// console.log(daysOfWeek);
+// console.log(firstHalf);
+
+/** Removing an item in the middle**/
+//NEED CLASS EXAMPLE : this one written incorrectly, couldn't keep up
+// const indexOfThursday = daysOfWeek.indexOf("Thursday");
+// const lastHalf = daysOfWeek.slice(indexOfThursday + 1);
+// daysOfWeek = daysOfWeek.slice(0, indexOfThursday).join(',') + ',';
+// daysOfWeek += lastHalf.join(',');
+// daysOfWeek = daysOfWeek.split(',');
+
