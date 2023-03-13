9f2bf50319e207f65ba77411af0998e0d2fbb78e
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-03-13 12:05:57 -0500

    Adding BOM lesson & exercise

---
+++ js/bom-lesson.js
@@ -0,0 +1,25 @@
+/**COUNTER --- make sure to put when to clear**/
+let count = 0;
+let max = 10;
+let interval = 1000;
+//
+// let intervalId = setInterval(function (){
+//     if (count >= max){
+//         clearInterval(intervalId);
+//         console.log('All Done!');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count);
+//     }
+// }, interval);
+
+/**TIMEOUT**/
+// console.log("5 seconds starts now!")
+// setTimeout(function (){
+//     console.log("It's been 5 seconds!")
+// }, 5000);
+
+/**REDIRECT**/
+// setTimeout(function (){
+//      window.location.href = 'https://google.com';
+// }, 10000);
