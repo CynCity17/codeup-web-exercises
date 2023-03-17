ff57c1ff0f181c1c79a67de5a571fd15b4f5750a
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-03-17 15:48:51 -0500

    adding jQuery moust events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,25 @@
 // $('.codeup').css('border', '1px solid red');
 
-$("li").css('font-size', '20px');
+// $("li").css('font-size', '20px');
+//
+// $("h1, p, li").css('background-color', 'pink');
+//
+// let header = $('h1').html();
+// alert(header);
 
-$("h1, p, li").css('background-color', 'pink');
+$('h1').click(function() {
+    $(this).css('background-color', 'pink');
+});
 
-let header = $('h1').html();
-alert(header);
+$('p').dblclick(function(e) {
+    $(this).css('font-size', '18px');
+});
+
+$('li').hover(
+    function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'black');
+    }
+);
