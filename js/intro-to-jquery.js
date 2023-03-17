6ecfb9cab931fd1330a3bd950844d99935ef67e3
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-03-17 12:42:41 -0500

    Adding additional jQuery exercises and lesson

--- js/intro-to-jquery.js
+++ js/intro-to-jquery.js
@@ -6,3 +6,11 @@ jQuery("h3").click(function(){
 $("h3").click(function(){
     $(this).next().slideToggle(500);
 });
+
+// $("#Library").text("jQuery is a Javascript library");
+
+const $libraryReference = $("#Library");
+let libraryText = $("#Library").text();
+console.log(libraryText);
+$libraryReference.text(`${libraryText} is library`);
+
--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,8 @@
-$(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $('.codeup').css('border', '1px solid red');
+
+$("li").css('font-size', '20px');
+
+$("h1, p, li").css('background-color', 'pink');
+
+let header = $('h1').html();
+alert(header);
