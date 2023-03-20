d102c6b4147aeb14523b2ed17360bc0348a2f0e2
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-03-20 15:27:20 -0500

    jQuery faq exercise

---
+++ js/jquery_faq.js
@@ -0,0 +1,8 @@
+$('dt').hover(function (event) {
+        $('dd').toggleClass('invisible');
+    });
+
+/**CLASS EXAMPLE PER INSTRUCTIONS OF EXERCISE TO USE BUTTON**/
+// $('button').on('click', function (){
+//     $('dd').toggleClass('invisible');
+// });
