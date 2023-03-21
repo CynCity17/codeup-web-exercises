f752c415f3c589cb0a38a4de8824aa4ab0b753ef
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-03-21 13:53:12 -0500

    add jQuery FAQ Exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -6,3 +6,24 @@ $('dt').hover(function (event) {
 // $('button').on('click', function (){
 //     $('dd').toggleClass('invisible');
 // });
+
+//DOES NOT WORK YET, tried to apply from class example
+// $('.list-button').on('click', function (){
+//     $('#national-parks-heading').find('ul li:last-child').toggleClass('highlight');
+// });
+
+/**My Incomplete attempt**/
+//     $('li').each(function(index) {
+//         if (index === 4 || index === 9) {
+//             $(this).css('background-color', '#FF0');
+//         }
+// });
+
+
+$('h3').on('click', function (){
+    $('ul').children().css('font-weight', 'bold');
+});
+
+$('#national-parks li, #state-parks-texas li').on('click', function (){
+    $(this).siblings().first().css('color', 'blue');
+});
