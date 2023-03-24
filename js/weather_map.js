cd612e924b834390717e68e60a9a483c39f4c620
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-03-24 14:00:46 -0500

    adding the start of weather project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,14 +1,62 @@
-const getForecast = async (weather) => {
-    try{
-        let response = await fetch(`http://api.openweathermap.org/data/2.5/weather${weather}`)
-        let data = await response.json();
-        return data;
-    } catch(error) {
-        console.log(error);
-    }
-};
+// const getForecast = async (weather) => {
+//     try{
+//         let response = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=29.423017&lon=-98.48527&limit=3&appid=${OPEN_WEATHER_API}`)
+//         let data = await response.json();
+//         return data;
+//     } catch(error) {
+//         console.log(error);
+//     }
+// };
+//
+// (async() =>{
+//     let weatherInfo = await getForecast();
+//     console.log(weatherInfo);
+// })();
 
-(async() =>{
-    let weatherInfo = await getForecast();
-    console.log(weatherInfo);
-})();
+
+
+
+
+
+
+
+
+
+
+
+
+// $.get("https://api.openweathermap.org/data/2.5/weather", {
+//     APPID: OPEN_WEATHER_API,
+//     q:     "San Antonio, US"
+// }).done(function(data) {
+//     console.log(data);
+// });
+//
+// const SALong = -98.48527;
+// const SALat = 29.423017;
+//
+// $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${SALat}&lon=${SALong}&appid=${OPEN_WEATHER_API}&units=imperial`).done(data=>{
+//     console.log(data);
+//     console.log("The temperature is: " + data.main.temp);
+//     $("#weather").html(`<p>The temperature is: ${data.main.temp}</p>`);
+// });
+//
+// $.get("http://api.openweathermap.org/data/2.5/forecast", {
+//     APPID: OPEN_WEATHER_API,
+//     lat:    SALat,
+//     lon:   SALong,
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log(data);
+//     data.list.forEach((forecast, index) => {
+//         if (index % 8 === 0 && index !== 0){
+//             const time = new Date(forecast.dt * 1000);
+//             console.log(time.getHours());
+//             console.log(time.getTime());
+//             console.log(time.getDay());
+//             console.log(forecast.dt_txt);
+//             console.log(forecast.weather[0].description);
+//         }
+//     });
+// });
+
