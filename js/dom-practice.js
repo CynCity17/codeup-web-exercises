0545d2d114deb03e96143b261de9a61d79892a9b
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-05-19 16:11:34 -0500

    changing layout css to add more defaults

--- js/dom-practice.js
+++ js/dom-practice.js
@@ -1,15 +1,29 @@
-function getCount (){
-    let items = document.querySelector('li');
-    let count = 0;
-    for(let i = 0; i < items.length; i++){
-        if (items.length[i]){
-            items.length++;
-        }
-    }
-    console.log(count);
+// const countBtn = document.querySelector(".count");
+// const paraInfo = document.querySelector("p");
+//
+// let countListener  = function getCount (){
+//     let ul = document.querySelector('ul');
+//     let items = ul.querySelectorAll('li');
+//     let count = 0;
+//     for(let i = 0; i < items.length; i++){
+//        count = items[i]++;
+//         }
+//         return count;
+//     }
+//
+//
+//
+// paraInfo.innerHTML = `Total list count is: ${countListener()}`
+// countBtn.addEventListener('click', countListener);
+let countList = function getCount(element){
+    let count = document.querySelectorAll('ul li'). length;
+    return count;
 }
+// let getCount = document.querySelectorAll('ul li').length;
+//console.log(getCount)
 
-const countBtn = document.querySelector(".count");
-countBtn.addEventListener("click", () => {
-    const paraInfo = document.querySelector('p').innerHTML = `Total list count is: ${getCount()}`;
-})
+const countBtn = document.querySelector('.count');
+// const paraInfo = document.querySelector('p .display');
+countBtn.addEventListener("click", countList);
+
+//paraInfo.innerHTML = `Total list count is: ${}`;
